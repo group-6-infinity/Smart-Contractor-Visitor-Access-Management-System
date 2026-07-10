a36cc6c9c5d118978f30e020549877ac5493ba09
naidew1 <nailha.dewi@student.president.ac.id>
2026-07-10 10:20:39 +0700

    fix: correct href destination in action.tsx

--- components/layouts/Home/action.tsx
+++ components/layouts/Home/action.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Action() {
   return (
     <Link
-      href="/test"
+      href="/track-status"
       className={cn(
         buttonVariants({ variant: "outline" }),
         "border-border text-foreground mx-auto mt-8 flex w-max cursor-pointer items-center justify-center rounded-sm border py-5 uppercase",
